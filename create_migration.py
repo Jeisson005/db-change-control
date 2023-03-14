9b22495a06971e552c42a67e516fca269a12e81b
Jeisson Rodrigo Piñeros Ramirez <jeisson005@hotmail.com>
2023-03-13 20:45:06 -0500

    initial

---
+++ create_migration.py
@@ -0,0 +1,33 @@
+import os
+
+folder = 'files'
+enc = 'utf-16-le'
+ignore = ['User'] + ['Table','Schema','Database']
+drop = ['PROCEDURE','FUNCTION','TYPE','VIEW']
+replace = ['StoredProcedure','UserDefinedFunction','UserDefinedTableType','View']
+
+files = os.listdir(folder)
+
+for file in files:
+    print(file)
+    rm = False
+    for i in ignore:
+        if file.endswith(f'.{i}.sql'):
+            os.remove(os.path.join(folder,file))
+            rm = True
+            break
+    if rm:
+        continue
+    with open(os.path.join(folder,file), 'r', encoding=enc) as fin:
+        data = fin.read()
+        for d in drop:
+            data = data.replace(f'DROP {d} ',f'DROP {d} IF EXISTS ')
+        data = data.splitlines(True)
+    with open(os.path.join(folder,file), 'w') as fout:
+        fout.writelines(data[3:])
+    for r in replace:
+        if file.endswith(f'.{r}.sql'):
+            migration = file.replace('dbo.','R__')
+            migration = migration.replace(f'.{r}.',f'_{r}.')
+            os.rename(os.path.join(folder,file),os.path.join(folder,migration))
+            break
